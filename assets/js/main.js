91a814111b54307cd66d4f9fcd090e83d0f7875b
AleFelix <alejandro.dunogent@gmail.com>
2018-05-27 19:29:51 -0300

    Fix thumbnails

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,13 +115,13 @@
 				$window.on('load', function() {
 
 					$('#two').poptrox({
-						caption: function($a) { return $a.next('h3').text(); },
+						// caption: function($a) { return $a.next('h3').text(); },
 						overlayColor: '#2c2c2c',
 						overlayOpacity: 0.85,
 						popupCloserText: '',
 						popupLoaderText: '',
 						selector: '.work-item a.image',
-						usePopupCaption: true,
+						// usePopupCaption: true,
 						usePopupDefaultStyling: false,
 						usePopupEasyClose: false,
 						usePopupNav: true,
